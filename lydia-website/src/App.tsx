1013ef9701fd210db642a68836160fe0088df8e6
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-08-31 16:46:07 +0100

    removed testAPI

--- lydia-website/src/App.tsx
+++ lydia-website/src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Timezone from './components/Timezone';
 import Updates from './components/Updates';
 import { Status } from './constants/Status';
-import { getStatuses, testAPI } from './api';
+import { getStatuses } from './api';
 import { hslFormat } from './util/colour';
 import StatusContext from './contexts/StatusContext';
 import TimeContext from './contexts/TimeContex';
