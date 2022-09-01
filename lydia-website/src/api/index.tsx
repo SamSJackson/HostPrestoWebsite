b8d0208a1caa1c11e1888c630a012b32ed034f8e
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-09-01 18:25:20 +0100

    parse to iso

--- lydia-website/src/api/index.tsx
+++ lydia-website/src/api/index.tsx
@@ -50,7 +50,7 @@ export async function addStatus(text : string, author : string) : Promise<Status
         id: generateID(),
         text: text,
         author: author, 
-        createdAt: convertDateToUTC(new Date()).toISOString(),
+        createdAt: convertDateToUTC(new Date()).toISOString().slice(0, 19).replace('T', ' '),
         country: country,
     }).then((response) => {
         console.log(response);
