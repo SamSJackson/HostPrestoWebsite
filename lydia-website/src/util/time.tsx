a035b866ef60966959b5367eef4b7d7b23463b7b
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-09-02 11:18:48 +0100

    fixed day issues on time

--- lydia-website/src/util/time.tsx
+++ lydia-website/src/util/time.tsx
@@ -1,5 +1,5 @@
 export function formatTime(date : Date) : string {
-    const [month, day, year] = [(date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1), (date.getDate() + 1 < 10 ? '0' : '') + (date.getDate() + 1), date.getFullYear()];
+    const [month, day, year] = [(date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1), (date.getDate() < 10 ? '0' : '') + date.getDate(), date.getFullYear()];
     const [hour, minutes] = [date.getHours(), (date.getMinutes() < 10 ? '0':'' ) + date.getMinutes()];
     const formattedTime = day + "/" + month + "/" + year + " " + hour + ":" + minutes; 
     return formattedTime;
